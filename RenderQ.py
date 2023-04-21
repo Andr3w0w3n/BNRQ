98d6c504898d7a2a1777611e42ad4f1b1cbff1db
Andrew Owen <47571927+Andr3w0w3n@users.noreply.github.com>
2023-04-21 11:16:20 -0600

    RenderQ.py update

--- RenderQ.py
+++ RenderQ.py
@@ -1,18 +1,44 @@
 import sys
 import os
-import nuke
+import subprocess
 from PyQt import QtWidgets, QtGui
-from PyQt.QtWidgets import QApplication, QMainWindow
-class MainWindow(QMainWindow):
-    """This creates the primary window for the render queue
+from PyQt.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QPushButton,
+    QLabel, QLineEdit, QVBoxLayout, QGridLayout
+)
 
+def run_render(script_path):
+    cmd = [nuke_exe,
+            "-ti",
+            render_script,
+            script_path
+            ]
+    proc = subprocess.Popen(cmd)
+    proc.communicate()
 
+class MainWindow(QWidget):
+    """
+    This creates the primary window for the render queue
     
     """
 
     def __init__(self):
         super().__init__()
+        self.resize(400, 400)
+        self.setWindowIcon(QIcon("ICON PATH GOES HERE"))
+        self.setWindowTitle("Nuke Render Queue")
+        self.setContentMargins(40, 40, 40, 40)
 
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        add_script_button = QPushButton("+")
+        layout.addWidget(add_script_button, 0, 0)
+
+        remove_script_button = QPushButton("-")
+        layout.addWidget(remove_script_button, 0, 1)
+
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
